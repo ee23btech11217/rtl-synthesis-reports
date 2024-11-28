6363550b525bd3c6fdb25a774f1a241628f641bf
Soham More <msoham2100@gmail.com>
2024-11-28 18:52:55 +0530

    added timing report parser

--- area_parser.py
+++ area_parser.py
@@ -27,11 +27,28 @@ def parse_area_report(filename):
                 maxHier = subblocks[-1][3]
     return [subblock for subblock in subblocks if subblock[-1] == maxHier]
 
+# returns tuple of timing data, data type, if data type is true then the given timing info is slack
 def parse_timing_report(filename):
+    time_regex = re.compile(r"Timing slack\s*\:\s*(.*)")
+    time_regex_uc = re.compile(r"out port(.*)")
     with open(filename, 'r') as f:
-        matches = re.findall('^Timing slack\s*:\s*(.*)$', f.read())
-        if len(matches) > 1:
+        file = f.read()
+        matches = re.search(time_regex, file)
+        if not matches:
+            print("[Report Parser] Note: unexpected report with no timing slack data, skipping report")
+            return None
+        if len(matches.groups()) > 1:
             print("[Report Parser] Note: unexpected report with multiple timing slacks, skipping report")
-        
+            return None
+        match = matches.group(1).strip()
 
-print(parse_area_report('UART/reports/1report_area.rpt'))
+        if match == 'UNCONSTRAINED':
+            matches = re.search(time_regex_uc, file)
+            return int(matches.group(0).strip().split()[-2]), True
+        
+        return int(match.removesuffix('ps')), False
+
+
+
+#print(parse_area_report('UART/reports/1report_area.rpt'))
+print(parse_timing_report('iscas85/reports/1report_timing.rpt'))
