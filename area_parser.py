e51bbc4e6bee214364b7edd0b5a784c86babbaf5
Soham More <msoham2100@gmail.com>
2024-11-28 19:37:55 +0530

    added benchmark report reader

--- area_parser.py
+++ area_parser.py
@@ -1,5 +1,20 @@
 import re
 
+naming_conv = {
+"1": "slow-100MHz",
+"2": "fast-100MHz",
+"3": "fast_hvt-100MHz",
+"4": "slow_hvt-100MHz",
+"5": "fast_lvt-100MHz",
+"6": "slow_lvt-100MHz",
+"7": "slow-400MHz",
+"8": "fast-400MHz",
+"9": "fast_hvt-400MHz",
+"10": "slow_hvt-400MHz",
+"11": "fast_lvt-400MHz",
+"12": "slow_lvt-400MHz",
+}
+
 def parse_area_table_row(line:str):
 
     leading_spaces = len(line) - len(line.lstrip())
@@ -25,7 +40,7 @@ def parse_area_report(filename):
             subblocks.append(parse_area_table_row(line))
             if subblocks[-1][3] > maxHier:
                 maxHier = subblocks[-1][3]
-    return [subblock for subblock in subblocks if subblock[-1] == maxHier]
+    return subblocks, maxHier
 
 # returns tuple of timing data, data type, if data type is true then the given timing info is slack
 def parse_timing_report(filename):
@@ -44,11 +59,54 @@ def parse_timing_report(filename):
 
         if match == 'UNCONSTRAINED':
             matches = re.search(time_regex_uc, file)
-            return int(matches.group(0).strip().split()[-2]), True
+            return int(matches.group(0).strip().split()[-2]), False
         
-        return int(match.removesuffix('ps')), False
+        return int(match.removesuffix('ps')), True
+def parse_power_report(filename):
+    subblocks = []
+    maxHier = 0
+    with open(filename, 'r') as f:
+        for line in f.readlines()[5:-1]:
+            if not line or line.isspace():
+                continue
+            if len(set(line)) == 1 and line[0] == '-':
+                continue
+            line = line.strip()
+            subblocks.append(( line.split()[-1], float(line.split()[-3]), int(line.split()[-2]) ))
+            if subblocks[-1][-1] > maxHier:
+                maxHier = subblocks[-1][-1]
+    return subblocks
 
+# parse all reports of the given folder, for given config
+def parse_reports(folder:str, config:str):
+    area_info, maxHier = parse_area_report(f'{folder}/reports/{config}report_area.rpt')
+    timing_info, timing_type = parse_timing_report(f'{folder}/reports/{config}report_timing.rpt')
+    power_info = parse_power_report(f'NEWPOWER/{folder}/reports/{config}report_power.rpt')
+
+    tot_area = area_info[0][2]
+    # timing_type is True, then this is slack
+    # find freq of clock and find the delay
+    if timing_type:
+        if '100MHz' in naming_conv[config]:
+            timing_info = 10000 - timing_info
+        elif '400MHz' in naming_conv[config]:
+            timing_info = 2500 - timing_info
+    tot_power = power_info[0][1]
+
+    area_subblocks = [ s for s in area_info if s[-1] == maxHier ]
+    power_subblocks = [ s for s in power_info if s[-1] == maxHier ]
+    
+    max_area = max(area_subblocks, key=lambda item: item[2])
+    max_power = max(power_subblocks, key=lambda item: item[1])
+
+    print(tot_area, timing_info, tot_power)
+    print(max_area, max_power)
 
 
 #print(parse_area_report('UART/reports/1report_area.rpt'))
-print(parse_timing_report('iscas85/reports/1report_timing.rpt'))
+#print(parse_timing_report('iscas85/reports/1report_timing.rpt'))
+#print(parse_power_report('NEWPOWER/UART/reports/1report_power.rpt'))
+
+parse_reports('itc99/b14', '1')
+
+
